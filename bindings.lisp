d08700837b8558d711606ba1ae583b2a39c64b1a
Martin Kielhorn <kielhorn.martin@gmail.com>
2014-05-26 11:49:35 +0200

    define fftw_plan_dft

--- bindings.lisp
+++ bindings.lisp
@@ -22,28 +22,24 @@
 (defcfun (%fftw_destroy_plan "fftw_destroy_plan")
     :void (plan plan-pointer))
 
-#+nil
+
 (defcfun (%fftw_plan_dft "fftw_plan_dft")
-    plan-pointer (rank ))
+    plan-pointer
+  (rank :int)
+  (n (:pointer :int))
+  (in :pointer) ;; complex double-float
+  (out :pointer) ;; complex double-float
+  (sign :int)
+  (flags :unsigned-int))
 
-
-;; (define-alien-routine fftw_plan_dft
-;;     plan
-;;   (rank int)
-;;   (n (* int))
-;;   (in (* double-float))	 ;; actually complex
-;;   (out (* double-float))	  ;; actually complex
-;;   (sign int)
-;;   (flags unsigned-int))
 
 ;; functions in the fftw3_threads library
-  
-;; (define-alien-routine ("fftw_init_threads" init-threads)
-;;     int)
 
-;; (define-alien-routine ("fftw_plan_with_nthreads" plan-with-nthreads)
-;;     void
-;;   (nthreads int))
+(defcfun (%fftw_init_threads "fftw_init_threads") :int)
+(defcfun (%fftw_plan_with_nthreads "fftw_plan_with_nthreads")
+    :void
+  (nthreads :int))
+
 
 
 ;; (progn
