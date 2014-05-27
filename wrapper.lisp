c58014f17d6e52e42a6c0f4d6a173f505f66f176
Martin Kielhorn <kielhorn.martin@gmail.com>
2014-05-27 07:10:34 +0200

    document how foreign data in ccl needs to be allocateded and start implementation

--- wrapper.lisp
+++ wrapper.lisp
@@ -12,7 +12,7 @@
 	    (if (= v -1)
 		1
 		v))
-  #-win32 1)
+  #-linux 1)
 
 (defun prepare-threads (&optional (n (get-number-processors)))
   "Initialize fftw3_threads to use n threads. On Linux n defaults to the number of processors."
@@ -25,15 +25,30 @@
 	 (out-d (array-displacement (if out out in))))
     (if (not (and in-d out-d))
 	(error "initially you should allocate data as a 1d array in lisp and then use displacement.")
-	(let* ((rank (array-rank in))
-	       (dims-in (make-array rank :element-type '(signed-byte 32)
-				    :initial-contents (array-dimensions in))))
-	  ;; this should work for 
-	  (with-pointer-to-vector-data (in-sap in-d)
-	    (with-pointer-to-vector-data (out-sap out-d)
-	      (with-pointer-to-vector-data (dims-sap dims-in)
-		(%fftw_plan_dft rank dims-sap in-sap out-sap
-			       +forward+ +estimate+))))))))
+	(let* ((rank (array-rank in)))
+	  ;;  http://ccl.clozure.com/ccl-documentation.html#Tutorial--Allocating-Foreign-Data-on-the-Lisp-Heap
+	  ;; for clozure cl, foreign objects on the heap must be
+	  ;; allocated on a special heap and freed after use. as far
+	  ;; in my opinion the implementation of
+	  ;; cffi:make-shareable-byte-vector in cffi 0.12.0 is
+	  ;; actually wrong. it just returns an array produced by
+	  ;; make-array. but it should use make-heap-ivector and later
+	  ;; free it. however, as i understand, the semantics of cffi
+	  ;; do not cover this.
+	  
+	  #+ccl
+	  (let ((dims-in (ccl::make-heap-ivector rank '(signed-byte 32))))
+	    (loop for i below rank and j in (array-dimensions in) do
+		 (setf (aref dims-in i) j))
+	    )
+	  #+sbcl
+	  (let ((dims-in (make-array rank :element-type '(signed-byte 32)
+				     :initial-contents (array-dimensions in))))
+	   (with-pointer-to-vector-data (in-sap in-d)
+	     (with-pointer-to-vector-data (out-sap out-d)
+	       (with-pointer-to-vector-data (dims-sap dims-in)
+		 (%fftw_plan_dft rank dims-sap in-sap out-sap
+				 +forward+ +estimate+)))))))))
 
 (defun ft (in)
   "Plan and execute an out-of-place Fourier transform of the array 'in'. The input array 'in' must be a displaced one-dimensional array."
