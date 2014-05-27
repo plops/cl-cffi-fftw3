5067e4a5bb42012f005de780b426ed11e4cee666
Martin Kielhorn <kielhorn.martin@gmail.com>
2014-05-27 07:58:46 +0200

    think more about how to get fast speed of ffts in ccl. ivectors can not be complex. but i can use array displacements to create multidimnesional vectors. i think i will use double-float ivectors and use dimensions h w 2 to be able to treat them as complex

--- wrapper.lisp
+++ wrapper.lisp
@@ -19,7 +19,7 @@
   (%fftw_init_threads)
   (%fftw_plan_with_nthreads n))
 
-(defun plan (in &optional out)
+(defun plan (in &optional out dims)
   (declare (type (array (complex double-float) *) in))
   (let* ((in-d (array-displacement in))
 	 (out-d (array-displacement (if out out in))))
@@ -37,9 +37,33 @@
 	  ;; do not cover this.
 	  
 	  #+ccl
-	  (let ((dims-in (ccl::make-heap-ivector rank '(signed-byte 32))))
-	    (loop for i below rank and j in (array-dimensions in) do
-		 (setf (aref dims-in i) j))
+	  (progn
+	    (when (and out (not (ccl::%heap-ivector-p out)))
+	      (error "ccl only supports reusing the output array, if it is a heap-ivector"))
+	    (let ((dims-in-foreign (ccl::make-heap-ivector rank '(signed-byte 32)))
+		  (dims-in (if (ccl::%heap-ivector-p in)
+			       dims
+			       (array-dimensions in))))
+	      (loop for i below rank and j in dims-in do
+		   (setf (aref dims-in-foreign i) j))
+	      
+	      (let ((in-foreign (if (ccl::%heap-ivector-p in)
+				    in
+				    (ccl::make-heap-ivector (array-total-size in) '(complex double-float))))
+		    (out-foreign (if (ccl::%heap-ivector-p out)
+				    out
+				    (ccl::make-heap-ivector (array-total-size in) '(complex double-float)))))
+
+		(unless (ccl::%heap-ivector-p in) ;; only copy data, if in isn't already a heap ivector
+		  (let ((in1 (make-array (array-total-size in) :element-type '(complex double-float)
+					 :displaced-to in)))
+		    (dotimes (i (length in1))
+		      (setf (aref in-foreign i) (aref in1 i)))))
+		(prog1
+		    (values (%fftw_plan_dft rank dims-in-foreign in-foreign out-foreign
+					    +forward+ +estimate+)
+			    in-foreign out-foreign)
+		  (ccl:dispose-heap-ivector dims-in-foreign))))
 	    )
 	  #+sbcl
 	  (let ((dims-in (make-array rank :element-type '(signed-byte 32)
@@ -50,22 +74,47 @@
 		 (%fftw_plan_dft rank dims-sap in-sap out-sap
 				 +forward+ +estimate+)))))))))
 
-(defun ft (in)
-  "Plan and execute an out-of-place Fourier transform of the array 'in'. The input array 'in' must be a displaced one-dimensional array."
+
+(defparameter *bla* (ccl:make-heap-ivector (* 4 4 2) 'double-float))
+
+(ccl:dispose-heap-ivector *bla*)
+
+(make-array (list 4 4) :element-type 'double-float :displaced-to *bla*)
+
+(defun ft (in &optional out)
+  "Plan and execute an out-of-place Fourier transform of the array
+'in'. SBCL allows to call the foreign function without copying the
+data but the input array 'in' must be a displaced one-dimensional
+array. In CCL, the input array and the output array are allocated on
+the foreign heap, i.e. if you don't use ccl:make-heap-ivector to
+allocate the arrays, the input and output data must be copied."
   (declare (type (array (complex double-float) *) in))
   (let* ((out1 (make-array (array-total-size in) :element-type '(complex double-float)))
 	 (out  (make-array (array-dimensions in) :element-type '(complex double-float)
-			   :displaced-to out1))
-	 (in1 (array-displacement in)))
-    (progn ;if (and in1 (equal '(complex double-float) (array-element-type in)))
-      (progn
-	  (with-pointer-to-vector-data (in-sap in1)
-	    #+sbcl (declare (ignore in-sap)) ;; i just do this in order to pin the array
-	    (with-pointer-to-vector-data (out-sap out1)
-	      #+sbcl (declare (ignore out-sap))
-	      (let ((plan (plan in out)))
-		(%fftw_execute plan))))
-	  out)
-      ; (error "input array is not displaced to 1d array. I can't work with this.")
-      )))
+			   :displaced-to out1)))
+    #+ccl
+    (let ((in1 (make-array (array-total-size in) :element-type '(complex double-float)
+			   :displaced-to in))
+	  (in-foreign (if (ccl::%heap-ivector-p in)
+			  in
+			  (ccl::make-heap-ivector (array-total-size in) '(complex double-float))))
+	  (out-foreign (if (ccl::%heap-ivector-p out)
+			   out
+			  (ccl::make-heap-ivector (array-total-size in) '(complex double-float)))))
+      (dotimes (i (length in1))
+	(setf (aref in-foreign i) (aref in1 i)))
+      )
+    #+sbcl
+    (let ((in1 (array-displacement in)))
+      (if (and in1 (equal '(complex double-float) (array-element-type in)))
+	 (progn
+	   (with-pointer-to-vector-data (in-sap in1)
+	     (declare (ignore in-sap)) ;; i just do this in order to pin the array
+	     (with-pointer-to-vector-data (out-sap out1)
+	       (declare (ignore out-sap))
+	       (let ((plan (plan in out)))
+		 (%fftw_execute plan))))
+	   out)
+	 (error "input array is not displaced to 1d array. I can't work with this.")))
+    ))
 
